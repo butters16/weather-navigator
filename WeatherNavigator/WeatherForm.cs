1a1851e6415466b6f56e5f56d34e523cb2d0a7bb
John Keith <johnkeith16@gmail.com>
2014-02-11 07:22:28 -0500

    Initial message box.

--- WeatherNavigator/WeatherForm.cs
+++ WeatherNavigator/WeatherForm.cs
@@ -37,23 +37,30 @@ namespace WeatherNavigator
                 //*[@id="wx-forecast-container"]/div[1]/div[2]/div[7]/div[1]/span[1] (ex: <span itemprop="temperature-fahrenheit">58</span>)
                 //*[@id="wx-forecast-container"]/div[1]/div[2]/div[8]/div[1] (ex: <div class="wx-temperature">75<span class="wx-degrees">°</span></div>)
                 //*[@id="wx-forecast-container"]/div[1]/div[2]/div[9]/div[1] (ex: <div class="wx-temperature">51<span class="wx-degrees">°</span></div>)
-                var temp1 = top.Div(1).Div(2).Div(7).Div(1).Span(1);
-                var temp2 = top.Div(1).Div(2).Div(8).Div(1);
-                var temp3 = top.Div(1).Div(2).Div(9).Div(1);
+                var temp_today = top.Div(1).Div(2).Div(8).Div(1).InnerText;
+                var temp_tonight = top.Div(1).Div(2).Div(9).Div(1).InnerText;
 
                 //*[@id="wx-forecast-container"]/div[1]/div[2]/div[16]/div[1]/div/div/span (ex: <span class="wx-temp">Calm</span>)
                 //*[@id="wx-forecast-container"]/div[1]/div[2]/div[16]/div[2]/div/div[2] (ex: <div class="wx-wind-label">E at 6 mph</div>)
                 //*[@id="wx-forecast-container"]/div[1]/div[2]/div[16]/div[3]/div/div[2] (ex: <div class="wx-wind-label">NE at 7 mph</div>)
-                var wind1 = top.Div(1).Div(2).Div(16).Div(1).Div(1).Div(1).Span(1);
-                var wind2 = top.Div(1).Div(2).Div(16).Div(2).Div(1).Div(2);
-                var wind3 = top.Div(1).Div(2).Div(16).Div(3).Div(1).Div(2);
+                var wind_today = top.Div(1).Div(2).Div(16).Div(2).Div(1).Div(2).InnerText;
+                var wind_tonight = top.Div(1).Div(2).Div(16).Div(3).Div(1).Div(2).InnerText;
 
                 //*[@id="wx-forecast-container"]/div[1]/div[2]/div[16]/div[4]/div (ex: <div class="wx-data">100%</div>)
                 //*[@id="wx-forecast-container"]/div[1]/div[2]/div[16]/div[5]/div (ex: <div class="wx-data">59%</div>)
                 //*[@id="wx-forecast-container"]/div[1]/div[2]/div[16]/div[6]/div (ex: <div class="wx-data">88%</div>)
-                var humid1 = top.Div(1).Div(2).Div(16).Div(4).Div(1);
-                var humid2 = top.Div(1).Div(2).Div(16).Div(5).Div(1);
-                var humid3 = top.Div(1).Div(2).Div(16).Div(6).Div(1);
+                var humid_today = top.Div(1).Div(2).Div(16).Div(5).Div(1).InnerText;
+                var humid_tonight = top.Div(1).Div(2).Div(16).Div(6).Div(1).InnerText;
+
+                StringBuilder builder = new StringBuilder();
+                builder.Append("Today Temperature: ").Append(temp_today).Append(Environment.NewLine); 
+                builder.Append("Today Wind: ").Append(wind_today).Append(Environment.NewLine);
+                builder.Append("Today Humidity: ").Append(humid_today).Append(Environment.NewLine);
+                builder.Append(Environment.NewLine);
+                builder.Append("Tonight Temperature: ").Append(temp_tonight).Append(Environment.NewLine); 
+                builder.Append("Tonight Wind: ").Append(wind_tonight).Append(Environment.NewLine);
+                builder.Append("Tonight Humidity: ").Append(humid_tonight).Append(Environment.NewLine);
+                MessageBox.Show(builder.ToString());
             }
         }
     }
