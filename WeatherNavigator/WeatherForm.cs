ce7d13a689a472196b3e1f2d399cd76563794e3d
John Keith <johnkeith16@gmail.com>
2014-02-11 06:58:50 -0500

    Form rename.

--- WeatherNavigator/WeatherForm.cs
+++ WeatherNavigator/WeatherForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WeatherNavigator
 {
-    public partial class Form1 : Form
+    public partial class WeatherForm : Form
     {
-        public Form1()
+        public WeatherForm()
         {
             InitializeComponent();
         }
